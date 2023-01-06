096431d6bfa25e6b607986d942e82cac01774773
SuJin <1127sujink@naver.com>
2023-01-06 11:41:09 +0900

    TIL : ts study

---
+++ practice/ts_with_react/README.md
@@ -0,0 +1,13 @@
+## Make TodoList. `Typescript` with React
+
+타입스크립트로 투두리스트 만들기!!
+![todolist](./public/todolist.PNG)
+
+### 느낀점
+`interface`를 생성해서 데이터 타입 정의용을 따로 만들어서 대입하는 형식이 독특하면서 가독성이 좋아보인다.
+
+타입을 지정해주면서 코드를 짜는게 변수 선언시 타입을 미리 정하는 C언어가 많이 생각난다. (`int`, `char`, `string` 등등..)
+
+<br><br>
+
+[참고] https://youtu.be/bjnW2NLAofI
---
+++ practice/ts_with_react/src/App.tsx
@@ -0,0 +1,67 @@
+import React, { FC, ChangeEvent, useState } from "react";
+import "./App.css";
+import { ITask } from "./Interfaces";
+import TodoTask from "./Components/TodoTask";
+
+const App: FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [deadline, setDeadline] = useState<number>(0);
+  const [todoList, setTodoList] = useState<ITask[]>([]);
+
+  // return type = void
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    if (event.target.name === "task") {
+      setTask(event.target.value);
+    } else {
+      setDeadline(Number(event.target.value));
+    }
+  };
+
+  const addTask = (): void => {
+    const newTask = { taskName: task, deadline: deadline };
+    setTodoList([...todoList, newTask]);
+
+    // reset
+    setTask("");
+    setDeadline(0);
+  };
+
+  const completeTask = (taskNameToDelete: string): void => {
+    setTodoList(
+      todoList.filter((task) => {
+        return task.taskName != taskNameToDelete;
+      })
+    );
+  };
+
+  return (
+    <div className="App">
+      <div className="header">
+        <div className="inputContainer">
+          <input
+            type="text"
+            name="task"
+            placeholder="할일을 입력하세요"
+            value={task}
+            onChange={handleChange}
+          ></input>
+          <input
+            type="number"
+            placeholder="Deadline (in Days)..."
+            name="deadline"
+            value={deadline}
+            onChange={handleChange}
+          />
+        </div>
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className="todoList">
+        {todoList.map((task: ITask, key: number) => {
+          return <TodoTask key={key} task={task} completeTask={completeTask} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default App;
